f3baf0aa39b1e81fdcd5fc183cc810b30918b52c
pangxie.wdy <pangxie.wdy@gmail.com>
2015-07-10 10:57:45 +0800

    修正package URL地址.

--- js/search.js
+++ js/search.js
@@ -1,10 +1,13 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+"use strict";
+
+const packageURLPrefix = 'http://npm.taobao.org/browse/keyword/';
+let url = packageURLPrefix + '{key}?type=json';
 
 $(function() {
-    var url = 'http://npm.taobao.org/browse/keyword/{key}?type=json'
-    var sendingRequest = false;
+    let sendingRequest = false;
     $('#doSearch').click(function() {
         var searchString = $('#keywordsOrName').val();
 
@@ -23,7 +26,7 @@ $(function() {
 
 /* 处理结果, 渲染模板 */
 function renderPage(data) {
-    var dataObj = {
+    let dataObj = {
         hasResult: data.keywords.length || data.packages.length || data.match,
         hasMatch: data.match,
         hasKeywords: data.keywords.length > 0,
@@ -34,10 +37,11 @@ function renderPage(data) {
         packages: data.packages,
         keywords: data.keywords,
         packagesCount: data.packages.length,
-        keywordsCount: data.keywords.length
+        keywordsCount: data.keywords.length,
+        packageURLPrefix: packageURLPrefix
     };
 
-    var compiled = dust.compile($('#page-tpl').val(), 'result');
+    let compiled = dust.compile($('#page-tpl').val(), 'result');
     dust.loadSource(compiled);
     dust.render('result', dataObj, function(err, out) {
         $('#result').html(out);
